85dbd68015d7b8c4c0045b232046906a15785459
Lou Knudsen <lou.knudsen.nz@gmail.com>
2023-03-03 10:51:33 +1300

    change port code

--- server/owner-routes.js
+++ server/owner-routes.js
@@ -5,55 +5,27 @@ const { dirname } = require('node:path')
 
 const router = express.Router()
 
+console.log('this owner route is working')
+
 router.get('/:id', (req, res) => {
   const idNumber = Number(req.params.id)
   console.log(idNumber)
 
-  fs.readFile (__dirname + '/data.json', 'utf-8')
+  fs.readFile(__dirname + '/data.json', 'utf-8')
 
-    .then ((data) => {
+    .then((data) => {
       const parsedInfo = JSON.parse(data)
-      const pokeMatch = parsedInfo.owners.find((element) => element.id === idNumber)
+      const pokeMatch = parsedInfo.owners.find(
+        (element) => element.id === idNumber
+      )
       res.render('owner-profile', pokeMatch)
     })
-  
-    .catch ((err) => {
+
+    .catch((err) => {
       res.status(404).send(err.message)
     })
-  })
+})
 
-
-    // router.get - for edit function
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// router.get - for edit function
 
 module.exports = router
-
-
-.then((data) => {
-  const parsedData = JSON.parse(data)
-  console.log(parsedData)
-  const idMatchValue = parsedData.puppies.find(
-    (element) => element.id === idNumber
-  )
-  console.log(idMatchValue)
-  res.render('details', idMatchValue)
-})
-.catch((err) => {
-  res.status(404).send(err.message)
-})
