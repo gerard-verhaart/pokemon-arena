c71069d8e56652ddeef92c098610896c07cb6386
Gerard Verhaart <121985303+gerard-verhaart@users.noreply.github.com>
2023-03-03 11:27:39 +1300

    manually added Ado and Reubens branch

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const hbs = require('express-handlebars')
-// const fs = require('node:fs/promises')
+const fs = require('node:fs/promises')
+const ownerRoute = require('./owner-routes.js')
 
 const server = express()
+server.use('/pokemon/', ownerRoute)
 
 // Server configuration
 const publicFolder = __dirname + '/public'
@@ -16,7 +18,13 @@ server.set('views', __dirname + '/views')
 
 // Your routes/router(s) should go here
 server.get('/', (req, res) => {
-  res.render('home')
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+    .then((data) => {
+      res.render('home', JSON.parse(data))
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
 })
 
 module.exports = server
