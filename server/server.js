e2529a0e91ba0c7dccf06e25f3b134775dca1c08
humaidiar <humaidiar123@gmail.com>
2023-03-02 19:14:27 +1300

    setting up server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+// const fs = require('node:fs/promises')
+
+const server = express()
+
+// Server configuration
+const publicFolder = __dirname + '/public'
+server.use(express.static(publicFolder))
+server.use(express.urlencoded({ extended: false }))
+
+// Handlebars configuration
+server.engine('hbs', hbs.engine({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+server.set('views', __dirname + '/views')
+
+// Your routes/router(s) should go here
+server.get('/', (req, res) => {
+  res.render('home')
+})
+
+module.exports = server
